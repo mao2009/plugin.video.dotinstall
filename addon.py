769fbc67df2d86414388ad392ea4eebd9b726259
mao2009 <en>
2018-09-04 19:26:28 +0900

    refactor

--- addon.py
+++ addon.py
@@ -1,10 +1,5 @@
 # coding: utf-8
 
-from bs4 import BeautifulSoup
-
-import xbmcgui
-import xbmcplugin
-
 from takoyaki import Takoyaki
 
 
@@ -13,20 +8,19 @@ class DotInstall(Takoyaki):
     BASE_URL = 'https://dotinstall.com/'
     LOGIN_URL = 'https://dotinstall.com/login.php'
 
-    def select_mode(self):
+    def run(self):
         modes = {
             'top_menu': self.top_menu,
             'lessons': self.lessons,
             'lesson': self.lesson,
             'select_source': self.select_source
         }
-        super(DotInstall, self).select_mode(modes)
+        self.select_mode(modes)
 
     def login(self):
         url = 'https://dotinstall.com/login'
-        html = self.download_html(url)
-        soup = BeautifulSoup(html)
-        token = soup.find('input', class_='one_time_token').get('value').encode('utf-8')
+        parser = self.parse_html(url)
+        token = parser.find('input', class_='one_time_token').get('value').encode('utf-8')
         query = {
             'mail': self.username,
             'password': self.password,
@@ -36,9 +30,8 @@ class DotInstall(Takoyaki):
 
     def lessons(self):
         url = self.url_join(self.BASE_URL, 'lessons')
-        html = self.download_html(url)
-        bs = BeautifulSoup(html)
-        elements = bs.find_all(class_='span8')
+        parser = self.parse_html(url)
+        elements = parser.find_all(class_='span8')
         num = int(self.params.get('element_num'))
         element = elements[num]
         lessons = element.find_all('a')
@@ -47,52 +40,47 @@ class DotInstall(Takoyaki):
         for lesson in lessons:
             link = self.url_join(self.BASE_URL, lesson.get('href'))
             title = lesson.text
-            li = xbmcgui.ListItem(title, iconImage=img_url)
-            query = {'mode': 'lesson', 'link': link, 'img_url': img_url}
-            url = self.build_url(query)
-            xbmcplugin.addDirectoryItem(handle=self.__handle__, url=url, listitem=li, isFolder=True)
-        xbmcplugin.endOfDirectory(self.__handle__)
+            list_item = {'label': title, 'iconImage': img_url}
+            params = {'mode': 'lesson', 'link': link, 'img_url': img_url}
+            self.add_directory(params, list_item)
+
+        self.end_of_directory()
 
     def lesson(self):
         url = self.params.get('link')
-        html = self.download_html(url)
-        bs = BeautifulSoup(html)
-        lessons = bs.find(id='lessons_list').find_all('a')
+        parser = self.parse_html(url)
+        lessons = parser.find(id='lessons_list').find_all('a')
         img_url = self.params.get('img_url')
         for lesson in lessons:
             link = self.url_join(self.BASE_URL, lesson.get('href'))
             title = lesson.text
-            li = xbmcgui.ListItem(title, iconImage=img_url)
-            query = {'mode': 'select_source', 'link': link, 'img_url': img_url}
-            url = self.build_url(query)
-            xbmcplugin.addDirectoryItem(handle=self.__handle__, url=url, listitem=li, isFolder=True)
-        xbmcplugin.endOfDirectory(self.__handle__)
+            list_item = {'label': title, 'iconImage': img_url}
+            params = {'mode': 'select_source', 'link': link, 'img_url': img_url}
+            self.add_directory(params, list_item)
+        self.end_of_directory()
 
     def select_source(self):
         if self.is_login:
             self.login()
 
         url = self.params.get('link')
-        html = self.download_html(url)
-        bs = BeautifulSoup(html)
-        sources = bs.find_all('source')
+        parser = self.parse_html(url)
+        sources = parser.find_all('source')
         img_url = self.params.get('img_url')
         for source in sources:
             link = self.url_join(self.BASE_URL, source.get('src'))
             title = source.get('data-res') + 'P ' + source.get('type')
-            li = xbmcgui.ListItem(title, iconImage=img_url)
-            query = {'mode': 'play_video', 'link': link, 'img_url': img_url}
-            url = self.build_url(query)
-            xbmcplugin.addDirectoryItem(handle=self.__handle__, url=link, listitem=li)
-        xbmcplugin.endOfDirectory(self.__handle__)
+            list_item = {'label': title, 'iconImage': img_url}
+            params = {'mode': 'play_video', 'link': link, 'img_url': img_url}
+            self.add_directory(params, list_item)
+        self.end_of_directory()
 
     def top_menu(self):
 
         url = self.url_join(self.BASE_URL, 'lessons')
-        html = self.download_html(url)
-        bs = BeautifulSoup(html)
+        parser = self.parse_html(url)
 
-        elements = bs.find_all(class_='span8')
+        elements = parser.find_all(class_='span8')
 
         for element_num, element in enumerate(elements):
             image_element = element.find('img')
@@ -104,11 +92,10 @@ class DotInstall(Takoyaki):
             img_url = image_element.get('src')
             title = title_element.text.encode('utf-8').replace('\n', '')
             img_url = self.url_join(self.BASE_URL, img_url)
-            li = xbmcgui.ListItem(title, iconImage=img_url)
-            query = {'mode': 'lessons', 'img_url': img_url, 'element_num': element_num}
-            url = self.build_url(query)
-            xbmcplugin.addDirectoryItem(handle=self.__handle__, url=url, listitem=li, isFolder=True)
-        xbmcplugin.endOfDirectory(self.__handle__)
+            list_item = {'label': title, 'iconImage': img_url}
+            params = {'mode': 'lessons', 'img_url': img_url, 'element_num': element_num}
+            self.add_directory(params, list_item)
+        self.end_of_directory()
 
 
 def main():
